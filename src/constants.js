e736ecdf8d8a87ea51830b07a75fa3b81ff75c90
Aiguemort <100524387+Aiguemort@users.noreply.github.com>
2022-02-27 15:52:15 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const CHANNEL_NAME = 'Antboy08';
+export const OAUTH_TOKEN = 'oauth:sr69iy9ihf32cfliik1jrovkl0rpbw';
+export const BOT_USERNAME = 'yourChannelName';
+export const BLOCKED_WORDS = [
+  '!play',
+  'cats',
+  'dogs',
+]
